52131fef06c564b18a0626b8387941502d34e628
yonjinoh <2020110869@dgu.ac.kr>
2025-03-29 19:28:47 +0900

    백엔드 초기설정

---
+++ backend/src/main/java/com/example/DevDumpling/DevDumplingApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevDumpling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevDumplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevDumplingApplication.class, args);
+	}
+
+}
