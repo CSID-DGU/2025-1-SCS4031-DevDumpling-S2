fdfc5663f8d09da5aa67be6b624e051e973e37c7
shin-yeseong <1020blue@dongguk.edu>
2025-04-06 20:45:38 +0900

    프론트 테스트

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import HomeScreen from './homescreen';
+import 'nativewind';
 
 const App = () => {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <View className="p-4">
-        <Text className="text-lg font-bold text-blue-500">하윙 ㅋㅋ 초기 설정 성공~</Text>
-      </View>
+    <SafeAreaView className="flex-1">
+      <HomeScreen />
     </SafeAreaView>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./homescreen.{js,jsx,ts,tsx}",
+    "./frontend/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
