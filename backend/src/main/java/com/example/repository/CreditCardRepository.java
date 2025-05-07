81968fb86229816ff572bea3b1c4cd4b6a434949
iSEViper <jklas187@naver.com>
2025-05-07 20:10:21 +0900

    체크카드/신용카드 api 조회 추가

--- backend/src/main/java/com/example/repository/CreditCardRepository.java
+++ backend/src/main/java/com/example/repository/CreditCardRepository.java
@@ -2,6 +2,8 @@ package com.example.repository;
 
 import com.example.entity.CreditCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
+    List<CreditCard> findByCompany(String company);
 } 
