177bc2026e68f42deaebb45a0500d33ff0768ae1
Heejin Kim <kimheejin@dgu.ac.kr>
2025-05-29 17:40:38 +0900

    청년우대상품 페이지 수정

--- frontend/src/screens/Products/Youth/YouthProductList.js
+++ frontend/src/screens/Products/Youth/YouthProductList.js
@@ -27,14 +27,14 @@ const YouthProduct = ({ navigation }) => {
                 }}>
 
                     {/* 추가 정보 안내란 */}
-                    <View className="items-left pb-5">
+                    <View className="items-left pb-10">
                         <TouchableOpacity onPress={navigateToAddYouthInfo}>
                             <Text className="text-2xl font-bold">추가 정보를 입력하면</Text>
                             <Text className="text-2xl font-bold">나에게 더 맞는 상품을 볼 수 있어요 →</Text>
                         </TouchableOpacity>
                     </View>
 
-                    {/* 1차 정보 수집 버튼 모음 */}
+                    {/* 1차 정보 수집 버튼 모음
                     <View className="flex-row justify-start gap-3 pb-5">
                         <TouchableOpacity
                             className="bg-[#F9F9F9] px-4 py-2 w-16 rounded-full shadow-md items-center justify-center">
@@ -48,10 +48,12 @@ const YouthProduct = ({ navigation }) => {
                             className="bg-[#F9F9F9] px-4 py-2 w-30 rounded-full shadow-md items-center justify-center">
                             <Text className="text-sm">개인 소득</Text>
                         </TouchableOpacity>
-                    </View>
+                    </View> */}
 
                     {/* 신청 가능 상품 리스트 */}
                     <View className="flex-col justify-center gap-3 mb-5">
+                        <Text className="text-lg font-bold">✅ 현재 가입 가능한 청년 우대 상품</Text>
+
                         <TouchableOpacity
                             onPress={navigateToYouthProduct}
                             className="bg-[#F9F9F9] p-4 rounded-2xl shadow-md">
@@ -68,7 +70,7 @@ const YouthProduct = ({ navigation }) => {
 
                     {/* 신청 불가능 상품 리스트 */}
                     <View className="flex-col justify-center gap-3">
-                        <Text className="text-lg font-bold">❌ 현재 가입 기간이 아니에요</Text>
+                        <Text className="text-lg font-bold">❌ 지금은 가입 기간이 아니에요</Text>
 
                         <TouchableOpacity className="bg-[#D9D9D9] p-4 rounded-2xl shadow-md">
                             <Text className="text-xs text-[#6D6D6D] mb-2">복지 / 문화</Text>
